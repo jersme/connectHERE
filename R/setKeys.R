12802e17b5cebd94ffaec3d0ec527b36efe8d404
Jeroen Smeets <jeroen.smeets@marketredesign.com>
2018-12-08 14:19:59 +0100

    Added the key checks

--- R/setKeys.R
+++ R/setKeys.R
@@ -5,7 +5,7 @@
 #' @return The checked App ID.
 #' @export
 #'
-#' @examples setID(id = "xxxxxxxxxxxxxxxxxxxxxx")
+#' @examples setID(id = "xxxxxxxxxxxxxxxxxxxx")
 setID <- function(id) {
 
   # check id argument
@@ -14,9 +14,9 @@ setID <- function(id) {
   }
 
   # check for id length
-  if (nchar(id) == 22) {
+  if (nchar(id) == 20) {
     message("Key length correct.")
-  } else if (nchar(id) != 22) {
+  } else if (nchar(id) != 20) {
     warning("Non standard key length.")
   } else {
     stop("Function error. Please contact package maintainer.")
@@ -24,4 +24,31 @@ setID <- function(id) {
 
   # return the keylength
   return(id)
+}
+
+#' Set HERE App ID
+#'
+#' @param code The project HERE App Code.
+#'
+#' @return The checked App Code.
+#' @export
+#'
+#' @examples setCode(code = "xxxxxxxxxxxxxxxxxxxxxx")
+setCode <- function(code) {
+  # check id argument
+  if (methods::hasArg(code) == FALSE) {
+    stop("Please fill in your HERE REST App Code.")
+  }
+
+  # check for id length
+  if (nchar(code) == 22) {
+    message("Key length correct.")
+  } else if (nchar(code) != 22) {
+    warning("Non standard key length.")
+  } else {
+    stop("Function error. Please contact package maintainer.")
+  }
+
+  # return the keylength
+  return(code)
 }
--- tests/testthat/test.setKeys.R
+++ tests/testthat/test.setKeys.R
@@ -3,9 +3,21 @@ context("setID")
 test_that("Check inputs", {
   expect_error(setID(), "Please fill in your HERE REST App ID.")
   expect_warning(setID(id = "xxx"), "Non standard key length.")
-  expect_message(setID(id = "xxxxxxxxxxxxxxxxxxxxxx"), "Key length correct.")
+  expect_message(setID(id = "xxxxxxxxxxxxxxxxxxxx"), "Key length correct.")
 })
 
 test_that("Check outputs", {
-  expect_equal(setID(id = "xxxxxxxxxxxxxxxxxxxxxx"), "xxxxxxxxxxxxxxxxxxxxxx")
+  expect_equal(setID(id = "xxxxxxxxxxxxxxxxxxxx"), "xxxxxxxxxxxxxxxxxxxx")
+})
+
+context("setCode")
+
+test_that("Check inputs", {
+  expect_error(setCode(), "Please fill in your HERE REST App Code.")
+  expect_warning(setCode(code = "xxx"), "Non standard key length.")
+  expect_message(setCode(code = "xxxxxxxxxxxxxxxxxxxxxx"), "Key length correct.")
+})
+
+test_that("Check outputs", {
+  expect_equal(setCode(code = "xxxxxxxxxxxxxxxxxxxxxx"), "xxxxxxxxxxxxxxxxxxxxxx")
 })
